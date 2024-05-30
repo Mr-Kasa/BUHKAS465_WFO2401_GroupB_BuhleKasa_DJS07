17d1209c6e58055fa021c2a5f7cfc69ecd13b6f4
Mr-Kasa <156632133+Mr-Kasa@users.noreply.github.com>
2024-05-30 10:30:02 +0200

    Created the header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import React from "react"
+import Header from "./components/Header"
+<link rel="stylesheet" href="style.css"></link>
+
 
 function App() {
 
   return (
     <>
-     
+     <Header></Header>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+export default function Header (){
+    return(
+        <header className="header">
+            
+                <img className="header--image" src="./src/components/troll-face.png" />
+       
+          
+                <h1 className="header--title">Meme Generator</h1>
+         
+            
+                <h3 className="header--project">React Course - Project 3</h3>
+
+        </header>
+    )
+}
